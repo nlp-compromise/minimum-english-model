f0b4c85d6b1150bbe66aa45cf0d3cf115c2dc80a
spencer kelly <spencerkelly86@gmail.com>
2021-12-24 16:46:57 -0500

    done for the day

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { learn, convert, compress, uncompress } from '/Users/spencer/mountain/suffix-thumb/src/index.js'
 import fileSize from './fileSize.js'
+
+import JJR from '../pairs/JJR.js'
 import JJS from '../pairs/JJS.js'
+import RBR from '../pairs/RBR.js'
 import VBD from '../pairs/VBD.js'
+import VBG from '../pairs/VBG.js'
+import VBN from '../pairs/VBN.js'
+import VBZ from '../pairs/VBZ.js'
 
 const allRules = (model) => {
   let arr = []
@@ -19,15 +25,18 @@ const compute = function (wordPairs, tag) {
   console.log('  ', arr.length, `words ${fileSize(JSON.stringify(wordPairs))}`)
   console.log('  ', allRules(model).length, 'rules', Object.keys(model.exceptions).length, 'exceptions')
   model = compress(model)
+  // console.log(model)
   // console.log(model.rules)
   let res = JSON.stringify(model)
+  // console.log(res)
   console.log(fileSize(res))
 }
 
-compute(VBD, 'JJR')
+
+compute(JJR, 'JJR')
 compute(JJS, 'JJS')
-compute(JJS, 'RBR')
+compute(RBR, 'RBR')
 compute(VBD, 'VBD')
-compute(VBD, 'VBG')
-compute(VBD, 'VBN')
-compute(VBD, 'VBZ')
+compute(VBG, 'VBG')
+compute(VBN, 'VBN')
+compute(VBZ, 'VBZ')
