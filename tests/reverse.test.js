4c6cecef1049162b25575aee447755599c42c4df
spencer kelly <spencerkelly86@gmail.com>
2021-12-30 10:57:13 -0500

    24 thousand tests passing

---
+++ tests/reverse.test.js
@@ -0,0 +1,36 @@
+import test from 'tape'
+import { learn, convert, compress, uncompress, reverse, validate } from '/Users/spencer/mountain/suffix-thumb/src/index.js'
+
+import JJR from '../pairs/JJR.js'
+import JJS from '../pairs/JJS.js'
+import RBR from '../pairs/RBR.js'
+import VBD from '../pairs/VBD.js'
+import VBG from '../pairs/VBG.js'
+import VBN from '../pairs/VBN.js'
+import VBZ from '../pairs/VBZ.js'
+
+let all = [
+  [JJR, 'JJR'],
+  [JJS, 'JJS'],
+  [RBR, 'RBR'],
+  [VBD, 'VBD'],
+  [VBG, 'VBG'],
+  [VBN, 'VBN'],
+  [VBZ, 'VBZ']
+]
+
+test('reverse:', function (t) {
+  all.forEach(a => {
+    let [words, name] = a
+    let pairs = Object.entries(words)
+    pairs = validate(pairs, { inverse: true })
+    let model = learn(pairs)
+    let rev = reverse(model)
+    pairs.forEach((a) => {
+      let out = convert(a[1], rev)
+      t.equal(out, a[0], `[${name}] '${a[0]}' -> '${out}'`)
+    })
+  })
+  t.end()
+})
+
